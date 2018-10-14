55e1177e6ed3975cba7d109bfc32d618b02db4e7
ohtomi <ohtomi.kenichi@gmail.com>
2018-10-14 19:59:48 +0900

    Add zuul proxy

--- src/main/kotlin/com/github/ohtomi/demo/DemoApplication.kt
+++ src/main/kotlin/com/github/ohtomi/demo/DemoApplication.kt
@@ -2,16 +2,25 @@ package com.github.ohtomi.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+import org.springframework.cloud.netflix.zuul.filters.post.LocationRewriteFilter
+import org.springframework.context.annotation.Bean
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@EnableZuulProxy
 @RestController
 @SpringBootApplication
 class DemoApplication {
 
-    @RequestMapping(value = ["/"])
+    @RequestMapping(value = ["/greet"])
     fun handle(): String {
         return "hello"
+    }
+
+    @Bean
+    fun locationRewriteFilter(): LocationRewriteFilter {
+        return LocationRewriteFilter()
     }
 }
 
